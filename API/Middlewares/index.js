06b223e7d86df18b35024da1521932923308a9a2
Jaydev Dwivedi <jaydevd@zignuts.com>
2025-03-19 19:34:43 +0530

    Add files via upload

---
+++ API/Middlewares/index.js
@@ -0,0 +1,5 @@
+import isAuthenticated from "./isAuthenticated.js";
+
+module.exports = {
+    isAuthenticated
+}
---
+++ API/Middlewares/isAuthenticated.js
@@ -0,0 +1,79 @@
+import jwt from 'jsonwebtoken';
+import User from '../Models/User.js';
+
+export default async function isAuthenticated(req, res, next) {
+
+    try {
+        const secretKey = process.env.SECRET_KEY;
+
+        const authHeader = req.headers['authorization'];
+        const token = authHeader && authHeader.split(' ')[1];
+
+        if (token) {
+
+            // Verify JWT
+            const verifyToken = await jwt.verify(token, process.env.SECRET_KEY);
+
+            if (!verifyToken) {
+                res.json({
+                    status: '400',
+                    message: '',
+                    error: 'Invalid Token',
+                    data: ''
+                })
+            }
+
+            const user = await User.findOne({
+                where: { token: token, id: id },
+                attributes: ['name', 'email']
+            });
+
+            if (!user) {
+                res.json({
+                    status: '400',
+                    message: '',
+                    error: 'No user found',
+                    data: ''
+                });
+            }
+
+            if (token === user.token) {
+                res.json({
+                    status: '200',
+                    message: 'User found',
+                    error: '',
+                    data: ''
+                });
+            }
+            else {
+                res.json({
+                    status: '400',
+                    message: '',
+                    error: 'some error occurred',
+                    data: ''
+                });
+            }
+
+
+            if (user.isActive) {
+                req.user = user;
+                next();
+            }
+            else {
+                if (!user) res.json({
+                    status: '400',
+                    message: '',
+                    error: 'some error occurred',
+                    data: ''
+                });
+            }
+            next();
+        }
+        else {
+            console.log("Invalid user.");
+        }
+    }
+    catch (err) {
+        console.log("Something went wrong");
+    }
+}
